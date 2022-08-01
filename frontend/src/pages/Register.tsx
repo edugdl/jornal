22df2e9775283db9ca25490e9eea1c30d2c4f1bf
Gabriel Sartori Rangel <gabriel.sartorirangel@gmail.com>
2022-08-01 18:00:00 -0300

    Modelo chamada http

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,10 +1,26 @@
+
 export default function Register() {
+    
+    function getUser(){
+        const cep = document.getElementById('cep') as HTMLInputElement;
+        const cep_value = cep.value;
+        return fetch(`https://viacep.com.br/ws/${cep_value}/json/`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        })
+    }
+
     return (
         <div>
             <div>Crie sua conta</div>
-            <form action="teste" method="post">
-                <input type="submit" value="enviar" />
-            </form>
+            <input type="text" name="coisa" placeholder="Seu cep" id="cep" />
+            <button onClick={getUser}>clique aqui</button>
         </div>
     );
 }
