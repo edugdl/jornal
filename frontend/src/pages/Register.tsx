6fe474072736d90eea1e0816cf1a76e1682d172a
Gabriel Sartori Rangel <gabriel.sartorirangel@gmail.com>
2022-08-01 18:58:06 -0300

    desespero

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,33 +1,28 @@
+async function registerUser(){
+    const usuario = {
+        nome : 'eduardo' ,
+        senha : '123' ,
+        cep : '11018864989' ,
+        dataNascimento : '2005-07-09' ,
+        email : 'eduardogdelazari2005@gmail.com' 
+    }
+    const response = await fetch('http://localhost:8085/registrarUsuario', {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(usuario)
+    })
+}
 
 export default function Register() {
-
-    function getUser(){
-        const cep = document.getElementById('cep') as HTMLInputElement;
-        const cep_value = cep.value;
-
-        let usuario = {
-            nome : 'eduardo' ,
-            senha : '123' ,
-            cep : '11018864989' ,
-            dataNascimento : '2005-07-09' ,
-            email : 'eduardogdelazari2005@gmail.com' 
-        }
-
-        return fetch(`127.0.0.1:8085/registrarUsuario`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body : JSON.stringify(usuario)
-        })
-        .then(()=>{console.log("Usuario registrado com sucesso")})
-    }
-
+    // const cep = document.getElementById('cep') as HTMLInputElement;
+    // const cep_value = cep.value;
     return(
         <div>
             <div>Crie sua conta</div>
             <input type="text" name="coisa" placeholder="Seu cep" id="cep" />
-            <button onClick={getUser}>clique aqui</button>
+            <button onClick={registerUser}>clique aqui</button>
         </div>
     );
 }
