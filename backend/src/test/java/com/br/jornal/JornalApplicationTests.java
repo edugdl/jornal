272991b375a1225da9cccb6258ee1003271aa0e7
Eduardo g de lazari <eduardogdelazari2005@gmail.com>
2022-08-01 17:05:49 -0300

    commit inicial backend

---
+++ backend/src/test/java/com/br/jornal/JornalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.jornal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JornalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
