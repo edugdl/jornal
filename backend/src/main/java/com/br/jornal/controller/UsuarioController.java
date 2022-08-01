629cfdb9537ad114c210c7f5c5a1642fc6f92abf
Eduardo g de lazari <eduardogdelazari2005@gmail.com>
2022-08-01 18:46:43 -0300

    tentando persistir no banco

--- backend/src/main/java/com/br/jornal/controller/UsuarioController.java
+++ backend/src/main/java/com/br/jornal/controller/UsuarioController.java
@@ -1,8 +1,10 @@
 package com.br.jornal.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.br.jornal.model.Usuario;
 import com.br.jornal.repository.UsuarioRepositorio;
 
 @RestController
@@ -10,4 +12,12 @@ public class UsuarioController {
     
     @Autowired
     UsuarioRepositorio usuarioRepositorio;
+
+    @PostMapping("/registrarUsuario")
+    public Usuario registrarUsuario(Usuario u){
+        System.out.println(u);
+        usuarioRepositorio.save(u);
+        System.out.println(u.getCep());
+        return u;
+    }   
 }
