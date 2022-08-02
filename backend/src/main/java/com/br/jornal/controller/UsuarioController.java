e1874899c5ffe3141cd1190aa0a9a9e6be4060a4
Gabriel Sartori Rangel <gabriel.sartorirangel@gmail.com>
2022-08-02 00:49:28 -0300

    Reimportando o modelo do usuário
    ?? n sei se vc tirou por acidente ou se teve um motivo específico

--- backend/src/main/java/com/br/jornal/controller/UsuarioController.java
+++ backend/src/main/java/com/br/jornal/controller/UsuarioController.java
@@ -10,9 +10,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.br.jornal.model.Usuario;
 import com.br.jornal.repository.UsuarioRepositorio;
 
 @RestController
