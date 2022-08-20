9bc8f4bee26c5022c3edfa54e091dd6de41eaff0
Gabriel Sartori Rangel <gabriel.sartorirangel@gmail.com>
2022-08-20 19:46:50 -0300

    Acesso temporário às páginas

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,23 @@
 export default function Home() {
     return (
-        <div>Home</div>
+        <div style ={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+        }}>
+            <a style={{
+                marginTop: '100px',
+                textDecoration: 'none',
+                color: 'white',
+                backgroundColor: 'purple',
+                borderRadius: '5px',
+            }} href='/register'>Cadastrar</a> <br/>
+            <a style={{
+                textDecoration: 'none',
+                color: 'black',
+                backgroundColor: 'orange',
+                borderRadius: '5px',
+            }} href='/login'>Login</a>
+        </div>
     );
 }
